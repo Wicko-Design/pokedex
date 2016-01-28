602d853ce6023214bb07fca5eace59e3ccdcfaba
Wicko-Design <david@wicko.co.uk>
2016-01-28 15:25:54 +0000

    Initial Commit

---
+++ Pokedex-by-Wicko/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex-by-Wicko
+//
+//  Created by iMac 27" on 28/01/2016.
+//  Copyright © 2016 Wicko Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
